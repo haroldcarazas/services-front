2918029701318535b403a2fed6a2d7cde7fc1fad
Harold Carazas <car23068@byui.edu>
2024-11-06 10:21:08 -0500

    crear servicio 50%

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -2,6 +2,7 @@ import { auth } from '@/auth';
 import DashboardLayout from '@/components/DashboardLayout';
 import TableServices from '@/components/TableServices';
 import { getServices } from '@/services/apiServices';
+import Link from 'next/link';
 
 async function Page() {
   const session = await auth();
@@ -10,9 +11,12 @@ async function Page() {
   return (
     <DashboardLayout>
       <section className='flex flex-col items-center p-2'>
-        <button className='bg-green-400 rounded-md px-3 py-2 text-white self-end'>
+        <Link
+          className='bg-green-400 rounded-md px-3 py-2 text-white self-end'
+          href='/services/new'
+        >
           Registrar servicio
-        </button>
+        </Link>
         <TableServices services={services} />
       </section>
     </DashboardLayout>
--- services/apiServices.js
+++ services/apiServices.js
@@ -8,3 +8,10 @@ export const getServices = async token => {
   });
   return response.data;
 };
+
+export const createService = async (token, service) => {
+  const response = await axiosInstance.post('/services', service, {
+    headers: { Authorization: token },
+  });
+  return response.data;
+};
